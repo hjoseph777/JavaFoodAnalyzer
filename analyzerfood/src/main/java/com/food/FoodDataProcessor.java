2fbc78b14750dad576cfdbcb71c1a4a75924b88c
hjoseph777 <hjoseph777@gmail.com>
2024-09-29 21:29:38 -0400

    Delete analyzerfood directory

--- analyzerfood/src/main/java/com/food/FoodDataProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.food;
-
-public class FoodDataProcessor {
-    public double calculateCaloriesPerServing(int fatgrams, int calories, int servings) {
-        double totalCalories = fatgrams * calories;
-        double caloriesPerServing = totalCalories / servings;
-        return Math.round(caloriesPerServing * 100.0) / 100.0; // Round off to 2 decimal places
-    }
-}
